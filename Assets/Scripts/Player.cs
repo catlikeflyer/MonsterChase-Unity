10b9d1f8e67136931661d56404979b2481dfc724
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2021-09-12 16:18:35 -0500

    Dude jumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,11 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private Rigidbody2D myBody;
-
     private Animator animator;
     private SpriteRenderer sr;
     private string WALK_ANIMATION = "Walk";
+    private bool isGrounded = true;
+    private string GROUND_TAG = "Ground";
 
     // Awake
     private void Awake() 
@@ -40,10 +41,14 @@ public class Player : MonoBehaviour
         AnimatePlayer();
     }
 
+    // Fixed update 
+    private void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
     void PlayerMoveKeyboard() {
         movementX = Input.GetAxisRaw("Horizontal");
-        Debug.Log("move x value is " + movementX);
-
         transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
     }
 
@@ -62,6 +67,22 @@ public class Player : MonoBehaviour
             // Not moving
             animator.SetBool(WALK_ANIMATION, false);
         }
+    }
 
+    void PlayerJump() {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            Debug.Log("jump");
+        }
+    }
+
+    // When player falls into the ground
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag(GROUND_TAG)) {
+            isGrounded = true;
+            Debug.Log("ground");
+        }
     }
 }
